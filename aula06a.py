fb9324cc0c0a347e65fc63b067c8b4555e09ac24
Ruan Figueiredo <ruanthiago67@gmail.com>
2022-02-05 15:52:19 -0300

    testes em python

---
+++ aula06a.py
@@ -0,0 +1,8 @@
+n1 = int(input('digite valor '))
+n2 = int(input('digite outro valor '))
+s = n1 + n2
+
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+
+
+
---
+++ aula07a.py
@@ -0,0 +1,6 @@
+n1 = int(input('opa, fale um número!'))
+n2 = int(input('diga outro'))
+s = n1 + n2
+d = n1 / n2
+m = n1*n2
+print('A soma é {}, \n a divisão é {} e \n a multiplicação é {}'.format(s, d, m))
