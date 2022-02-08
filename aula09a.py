c14f7e754635124bf16cc98fefa089e0a8753cce
Ruan Figueiredo <ruanthiago67@gmail.com>
2022-02-08 00:03:17 -0300

    aprendendo a manipular Strings

---
+++ aula09a.py
@@ -0,0 +1,28 @@
+# começando a aprender manipulação de textos
+
+"""fatiamento"""
+
+"""frase = 'curso em vídeo python'
+print(frase[3])"""
+
+"""frase = 'curso em video'
+print(frase[3:13])"""
+
+"""frase = 'curso em videos python'
+print(frase[1:15:2])"""
+
+"""análise"""
+
+"""frase = 'curso em videos python'
+print(frase.count('o'))"""
+
+"""frase = 'curso em videos pythonzera'
+print(len(frase))"""
+
+#continua em outro repositório de exercícios.
+
+
+
+
+
+
